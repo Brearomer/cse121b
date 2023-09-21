4282c69c4a5cdd3333bdb88569eb46fb2e395127
Brea21 <celestebromer@gmail.com>
2023-09-21 15:27:50 -0400

    changes week02 -completed

---
+++ w02-task/scripts/w02-task.js
@@ -0,0 +1,34 @@
+/* W02-Task - Profile Home Page */
+
+/* Step 1 - Setup type tasks - no code required */
+
+
+/* Step 2 - Variables */
+let fullName = 'Brea Romer';
+let currentYear = '2023';
+let profilePicture = 'images/profilepicture.jpg';
+
+
+/* Step 3 - Element Variables */
+const nameElement = document.getElementById('name');
+const foodElement = document.getElementById('food');
+const yearElement = document.querySelector('#year');
+const imageElement = document.querySelector('img');
+
+/* Step 4 - Adding Content */
+const link = document.getElementById('fullName');
+nameElement.innerHTML =`<strong>${fullName}</strong>`;
+yearElement.textContent= currentYear;
+imageElement.setAttribute('src', profilePicture);
+imageElement.setAttribute('alt',fullName);
+
+
+/* Step 5 - Array */
+let favoritefood =['pizza','crackconch','conchsalad','icecream','cake','crabandrice','hotwings','popcorn','candy','wingsandfries','strawberry','blueberry','chocolate'];
+foodElement.textContent=favoritefood
+favoritefood.push('sweet and sour chicken');
+foodElement.innerHTML += `<br>${favoritefood}`;
+favoritefood.shift();
+foodElement.innerHTML += `<br>${favoritefood}`;
+favoritefood.pop()
+foodElement.innerHTML +=`<br>${favoritefood}`;
