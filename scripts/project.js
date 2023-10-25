6836f43b03fc52210c3afe8713d353d7e8f543e2
Brea21 <celestebromer@gmail.com>
2023-10-24 20:30:08 -0400

    project not yet finish errors

--- scripts/project.js
+++ scripts/project.js
@@ -1,45 +1,32 @@
-const timeEl = document.getElementById('time');
-const dateEl = document.getElementById('date');
-const currentweatherItemsEl = document.getElementById
-('current-weather-items');
-const timezone = document.getElementById('time-zone');
-const countryEl = document.getElementById('country');
-const weatherforecastEl = document.getElementById('weather-forecast');
-const currentTempEl = document.getElementById('current-temp');
 
-const days =['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday']
-const months =['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
 
-const API_KEY ='https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m'
+const hourly = document.getElementById('hourly');
+const timezone = document.getElementById('time');
+const weatherList = document.getElementById('weatherList');
+const temperature_2m = document.getElementById('temperature_2m');
 
-setInterval(() =>{
-    const time = new Date();
-    const month = time.getMonth();
-    const date = time.getDate();
-    const day = time.getDay();
-    const hour = time.getHours();
-    const hourIn2HrFormat = hour >= 13 ? hour %12: hour
-    const minutes = time.getMinutes(); 
-    const ampm = hour >=12 ? 'PM' :'AM'
+const link = document.getElementById('weatherList');
 
-    timeEl.innerHTML = hoursIn12HrFormat + ':' + minutes+ ' ' +'<span id= "am-pm">'
 
-    dateEl.innerHTML = days[day] + ' ,' + date+ ' ' + months[month]     
 
-},1000);  
 
-function getWeatherData (){
-    navigator.geolocation.getCurrentPosition((success)=> {
+ 
 
-        let { latitude , longitude} = success.coords;
-
-        fetch('https://api.open-meteo.com/v1/forecast?${latitude}=52.52&longitude=13.41&hourly=temperature_2m'(API_KEY).then(res =>res.json()).then(data =>)
+const getWeather = async() =>{
+     const response = await fetch("https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m")
+        console.weatherList = await response.json();
+       console.log (weatherList);
+       console.log (weatherList.hourly.time);  
+       console.log(weatherList.hourly.temperature_2m);
+       
+ }; 
    
-         console.log(data)
-         showWeatherData(data);
-        })         
 
-    })
+  
 
-} 
-function 
+
+
+ getWeather();   
+
+
+
